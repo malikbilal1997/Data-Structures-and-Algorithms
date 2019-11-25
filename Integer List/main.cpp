7fd7c02d0fd504988a973293419c707ac7826a48
Malik Bilal <malikbilal1997@outlook.com>
2019-11-26 02:12:20 +0500

    Added a few comments

--- Integer List/main.cpp
+++ Integer List/main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
+
+// Including Integer List
 #include "list.hpp"
 
 int main() 
 {
+    // Declaring Int List Object
     IntList l;
+    // Operations performed on Int List
     l.push_back(10);
     l.push_back(11);
     l.push_back(12);
     l.push_back(13);
     l.push_back(14);
 
+    // Declaring an Iterator from Int List
     IntList::Iterator it;
     it = l.begin();
 
+    // Iterating the Int List
     while (it != l.end())
     {
         std::cout  << *it  << "\n";
